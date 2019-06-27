41f9d9a392969d734c1db11483cd09f1b58c1a7c
JacqueStar3 <JacqueStar3@github.com>
2019-06-27 16:00:21 -0500

    Lesson12

--- src/com/fundamentals/java/Truck.java
+++ src/com/fundamentals/java/Truck.java
@@ -1,4 +1,76 @@
 package com.fundamentals.java;
 
-public class Truck {
+
+import javax.swing.*;
+import java.awt.*;
+
+public class  Truck {
+     private String wheels;
+     private int windows;
+     private String box;
+     private String color;
+
+    public Truck() {
+        System.out.println("This is the default constructor");
+
+    }
+
+    public Truck(String color) {
+        this(color, "chrome", 28, "flat");
+        System.out.println("The truck is" + color);
+
+    }
+
+    public Truck(String color, String wheels, int window, String box) {
+
+        this.windows = windows;
+        this.box = box;
+        this.color = color;
+        this.wheels = wheels;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setBox(String getsetBox){
+        this.box = box;
+    }
+
+    public void setWheels(String getsetWheels){
+        this.wheels = wheels;
+    }
+
+    public void setWindows (String getwindows) {
+        this.windows = windows;
+    }
+
+    public String getWheels() {
+        return wheels;
+    }
+
+
+    public String getBox() {
+        return box;
+    }
+
+    public String getcolor() {
+        return color;
+    }
+
+    public int getwindows() {
+        return windows;
+    }
+
+    public void Driving() {
+       System.out.println("Driving");
+
+
+    }
 }
+
+
