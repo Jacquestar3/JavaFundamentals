219bfd21ae44eb709c9fb39961b522c8d6f9ea46
JacqueStar3 <JacqueStar3@github.com>
2019-07-01 09:10:07 -0500

    Lab

--- src/com/fundamentals/java/Condo.java
+++ src/com/fundamentals/java/Condo.java
@@ -1,4 +1,43 @@
 package com.fundamentals.java;
 
-public class Condo {
+public class Condo extends House {
+    private String balcony;
+
+    public void maintenence(){
+        System.out.println("will fix condo issues ");
+    }
+
+
+    public String getBalcony() {
+        return balcony;
+    }
+
+    public void setBalcony(String balcony) {
+        this.balcony = balcony;
+        }
+public Condo(String balcony,String doorColor, int windowSize, String foundation, String roofType){
+        super(doorColor, windowSize,foundation, roofType );
+        this.balcony = balcony;
 }
+public Condo(){
+        System.out.println("Condo default constructor");
+}
+
+public void doorOpenClose(){
+        System.out.println("My Condo door is open");
+}
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
--- src/com/fundamentals/java/Mylab3cont.java
+++ src/com/fundamentals/java/Mylab3cont.java
@@ -1,4 +1,24 @@
 package com.fundamentals.java;
 
 public class Mylab3cont {
+
+    public void labCont() {
+        int today = 0;
+        do {
+            if (today % 3 == 0) {
+                System.out.println(today);
+            }
+            today++;
+        } while (today < 50);
+
+
+    }
 }
+
+
+
+
+
+
+
+
