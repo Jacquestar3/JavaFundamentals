e0cbd2ed537eb04454a5607196469b183e73fd62
JacqueStar3 <JacqueStar3@github.com>
2019-07-02 15:47:36 -0500

    Lesson 16, 17, Lab

---
+++ src/com/fundamentals/java/Lesson16.java
@@ -0,0 +1,4 @@
+package com.fundamentals;
+
+public class Lesson16 {
+}
---
+++ src/com/fundamentals/lesson10/Lab3.java
@@ -0,0 +1,27 @@
+package com.fundamentals;
+
+public class Lab3 {
+}
+
+public void showGrade(char grad){
+
+    switch(grade){
+        case E:
+            return;
+        case V:
+            return,
+        case G:
+            return
+        case A:
+            return
+
+        case F:
+            return
+        default:
+           return "NOt a valid grade";
+
+
+    }
+}
+Lab3 lab3 = new Lab30();
+System.out.println(lab3.showGrade("G"));
